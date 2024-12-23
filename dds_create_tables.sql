aa4b3d12348e6aba52ee1224bacfe1680ce3dd32
Dmitry07 <dmitry.terenich@gmail.com>
2024-12-23 14:23:48 +0300

    Fix dds

--- dds_create_tables.sql
+++ dds_create_tables.sql
@@ -3,33 +3,16 @@ CREATE TABLE executors (
 );
 
 CREATE TABLE zones (
-    zone_name VARCHAR(255) PRIMARY KEY
+    zone_id VARCHAR(255) PRIMARY KEY
+    zone_name VARCHAR(255)
 );
 
-CREATE TYPE completed_or_cancelled_status AS ENUM ('cancelled', 'completed');
+CREATE TYPE order_execution_status AS ENUM ('assigned', 'acquired', 'cancelled', 'completed');
 
-CREATE TABLE completed_and_cancelled_orders (
+CREATE TABLE orders (
     order_id VARCHAR(255) NOT NULL,
     executor_id VARCHAR(255) NOT NULL,
-    execution_status completed_or_cancelled_status NOT NULL,
-    coin_coefficient DOUBLE PRECISION NOT NULL,
-    coin_bonus_amount DOUBLE PRECISION NOT NULL,
-    final_coin_amount DOUBLE PRECISION NOT NULL,
-    zone_name VARCHAR(255),
-    assign_time TIMESTAMP WITH TIME ZONE NOT NULL,
-    first_acquire_time TIMESTAMP WITH TIME ZONE,
-    cancel_time TIMESTAMP WITH TIME ZONE,
-    executor_rating FLOAT NOT NULL,
-
-    UNIQUE (order_id)
-);
-
-CREATE TYPE status_in_progress AS ENUM ('assigned', 'acquired', 'cancelled', 'completed');
-
-CREATE TABLE orders_in_progress (
-    order_id VARCHAR(255) NOT NULL,
-    executor_id VARCHAR(255) NOT NULL,
-    execution_status status_in_progress NOT NULL,
+    execution_status order_execution_status NOT NULL,
     coin_coefficient DOUBLE PRECISION NOT NULL,
     coin_bonus_amount DOUBLE PRECISION NOT NULL,
     final_coin_amount DOUBLE PRECISION NOT NULL,
