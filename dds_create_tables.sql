aae4a88481acdee5223899a68e500d1e9d2e9f89
Dmitry07 <dmitry.terenich@gmail.com>
2024-12-23 14:58:41 +0300

    Add relations to dds tables

--- dds_create_tables.sql
+++ dds_create_tables.sql
@@ -2,8 +2,12 @@ CREATE TABLE executors (
     executor_id BIGINT PRIMARY KEY
 );
 
+CREATE TABLE customers (
+    customer_id BIGINT PRIMARY KEY
+);
+
 CREATE TABLE zones (
-    zone_id BIGINT PRIMARY KEY
+    zone_id BIGINT PRIMARY KEY,
     zone_name VARCHAR(255)
 );
 
@@ -11,12 +15,13 @@ CREATE TYPE order_execution_status AS ENUM ('assigned', 'acquired', 'cancelled',
 
 CREATE TABLE orders (
     order_id BIGINT NOT NULL PRIMARY KEY,
-    executor_id BIGINT NOT NULL,
+    executor_id BIGINT NOT NULL REFERENCES executors(executor_id),
+    customer_id BIGINT NOT NULL REFERENCES customers(customer_id),
     execution_status order_execution_status NOT NULL,
     coin_coefficient DOUBLE PRECISION NOT NULL,
     coin_bonus_amount DOUBLE PRECISION NOT NULL,
     final_coin_amount DOUBLE PRECISION NOT NULL,
-    zone_name VARCHAR(255),
+    zone_id BIGINT NOT NULL REFERENCES zones(zone_id),
     assign_time TIMESTAMP WITH TIME ZONE NOT NULL,
     first_acquire_time TIMESTAMP WITH TIME ZONE,
     cancel_time TIMESTAMP WITH TIME ZONE,
